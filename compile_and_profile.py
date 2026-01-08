c1a363653a1e6129d6df6972fe333df72f64e279
Scott Jing <17432497+scottjingtt@users.noreply.github.com>
2026-01-07 17:24:15 -0800

    Change target runtime from tflite to qnn_lib_aarch64_android
    using qnn runtime to get similar results as AIHub official results for OpenCLIP. TFLite causes significantly large profiling latency

--- compile_and_profile.py
+++ compile_and_profile.py
@@ -22,7 +22,7 @@ def compile_model(model, device, input_specs):
         model=model,
         device=device,
         input_specs=input_specs,
-        options="--target_runtime tflite --truncate_64bit_io"
+        options="--target_runtime qnn_lib_aarch64_android --truncate_64bit_io"
     )
     return compile_job.job_id
 
